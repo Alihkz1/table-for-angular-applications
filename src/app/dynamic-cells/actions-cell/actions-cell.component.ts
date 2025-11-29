3b2d58b426a981f5f5e2bf5474519346ecbecd37
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2025-11-29 09:56:31 +0330

    refactor table

--- src/app/dynamic-cells/actions-cell/actions-cell.component.ts
+++ src/app/dynamic-cells/actions-cell/actions-cell.component.ts
@@ -1,16 +1,55 @@
-import { Component, EventEmitter, Input } from '@angular/core';
-import { IRowEvent } from '../../shared/model/IRowEvent.interface';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface ActionHeader {
+  title: string;
+  actionName: string;
+  fields: string[];
+}
 
 @Component({
   selector: 'app-actions-cell',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './actions-cell.component.html',
   styleUrl: './actions-cell.component.scss'
 })
-export class ActionsCellComponent {
+export class ActionsCellComponent implements OnDestroy {
   @Input() row: any;
-  @Input() onRowEvent: EventEmitter<IRowEvent>;
+  @Input() onRowEvent: EventEmitter<any>;
+  @Input() headers: ActionHeader[] = [];
+
+  mouseDownEventListenerRef: any;
+  public showColumnsMenu = false;
+
+  public actionHeaders: ActionHeader[] = [
+    {
+      title: 'ویرایش',
+      actionName: 'view_user',
+      fields: ['name', 'email', 'phone']
+    },
+    {
+      title: 'فعالیت انجام شد',
+      actionName: 'view_order',
+      fields: ['order_id', 'amount', 'status']
+    },
+    {
+      title: 'حذف',
+      actionName: 'view_payment',
+      fields: ['payment_method', 'transaction_id']
+    }
+  ];
+  
+  constructor() {
+    this.mouseDownEventListenerRef = document.addEventListener('mousedown', () => {
+      if (!this.showColumnsMenu) return
+      this.showColumnsMenu = false
+    })
+  }
+
+  ngOnDestroy(): void {
+    document.removeEventListener('mousedown', this.mouseDownEventListenerRef)
+  }
 
   onEvent(action: string) {
     this.onRowEvent.emit({
@@ -18,4 +57,14 @@ export class ActionsCellComponent {
       data: this.row
     })
   }
+
+  toggleColumnsMenu() {
+    this.showColumnsMenu = !this.showColumnsMenu;
+  }
+
+  closeColumnsMenu() {
+    this.showColumnsMenu = false;
+  }
+
 }
+
